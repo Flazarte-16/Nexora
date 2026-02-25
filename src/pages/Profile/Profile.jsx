8b3213ec5c2b68b9482c7b5dae3493c0e499eac7
cpasquali <conipasquali2@gmail.com>
2026-02-25 19:44:59 -0300

    cambios de estilo en la profile

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -4,26 +4,22 @@ export const Profile = () => {
   return (
     <main className="main main--profile">
       <section className="user-info">
-        <article className="profile-topbar">
-          <ion-icon name="arrow-back-outline"></ion-icon>
-          <article>
-            <h2 className="user-name">Constantino Pasquali</h2>
-            <h3 className="user-cant-post">2 posts</h3>
-          </article>
-        </article>
-        <img
-          src="https://images.unsplash.com/photo-1542831371-29b0f74f9713?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cHJvZ3JhbWFjaW9ufGVufDB8fDB8fHww"
-          alt="wall-photo"
-          className="wall-photo"
-        />
-        <img
-          src="https://media.licdn.com/dms/image/v2/D4D03AQFVhpA9Yxu8Og/profile-displayphoto-shrink_100_100/B4DZUj6bGcGkAU-/0/1740064273451?e=1773273600&v=beta&t=5hyUnpPAaSTaZUbN3LiReXz8q5EIv7LfsixbxB-xE90"
-          alt="Foto de perfil"
-          className="user-img"
-        />
+        <section
+          className="banner"
+          style={{
+            backgroundImage:
+              "url(https://images.unsplash.com/photo-1542831371-29b0f74f9713?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cHJvZ3JhbWFjaW9ufGVufDB8fDB8fHww)",
+          }}
+        ></section>
+
         <article className="user-details">
-          <h2>Constantino Pasquali</h2>
-          <h3>@cpasquali</h3>
+          <img
+            src="https://media.licdn.com/dms/image/v2/D4D03AQFVhpA9Yxu8Og/profile-displayphoto-shrink_100_100/B4DZUj6bGcGkAU-/0/1740064273451?e=1773273600&v=beta&t=5hyUnpPAaSTaZUbN3LiReXz8q5EIv7LfsixbxB-xE90"
+            alt="Foto de perfil"
+            className="user-img"
+          />
+          <h2 className="user-name">Constantino Pasquali</h2>
+          <h3 className="user-username">@cpasquali</h3>
           <p>
             Programmer in training with a strong interest in web development and
             modern technologies. Focused on learning and continuously improving
