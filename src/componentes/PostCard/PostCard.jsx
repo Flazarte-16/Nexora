ec29d20b94dd4c1fa2653e8b45140e1b3b09e691
Flazarte-16 <142759355+Flazarte-16@users.noreply.github.com>
2026-03-08 17:23:07 -0300

    css para los comentarios

--- src/componentes/PostCard/PostCard.jsx
+++ src/componentes/PostCard/PostCard.jsx
@@ -4,6 +4,7 @@ import { Link } from "wouter";
 
 export const PostCard = ({ post }) => {
   const [comments, setComments] = useState([]);
+  const [showComments, setShowComments] = useState(false);
 
   useEffect(() => {
     fetch(`http://localhost:3000/v1/comments/${post.id}`)
@@ -39,16 +40,23 @@ export const PostCard = ({ post }) => {
         )}
       </p>
       <article className="post-card-actions">
-        <button className="action-btn comments">
+        <button
+          className="action-btn comments"
+          onClick={() => setShowComments(!showComments)}
+        >
           <ion-icon className="sidebar-icon" name="chatbox-sharp"></ion-icon>
+          <span>{comments.length}</span>
         </button>
         <button className="action-btn likes">
           <ion-icon className="sidebar-icon" name="heart"></ion-icon>
         </button>
       </article>
-      <section>
+      <section
+        className={`post-card-comments ${showComments ? "show" : "hide"}`}
+      >
         {comments.map((comment) => (
-          <div key={comment.id}>
+          <div key={comment.id} className="comment">
+            <img src={post.user.image_url} alt={`foto de ${post.username}`} />
             <p>{comment.content}</p>
           </div>
         ))}
