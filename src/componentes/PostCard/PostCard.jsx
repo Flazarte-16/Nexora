4353d205fb63d3c36922bcef24ec11318310377f
cpasquali <conipasquali2@gmail.com>
2026-03-12 13:00:18 -0300

    se agregaron los comentarios, ademas de un input para crear uno nuevo

--- src/componentes/PostCard/PostCard.jsx
+++ src/componentes/PostCard/PostCard.jsx
@@ -1,16 +1,42 @@
 import { useEffect, useState } from "react";
 import "./PostCard.css";
 import { Link } from "wouter";
+import { useAuth } from "../../hooks/useAuth";
 
 export const PostCard = ({ post }) => {
   const [comments, setComments] = useState([]);
   const [showComments, setShowComments] = useState(false);
+  const [commentContent, setCommentContent] = useState("");
+  const { user } = useAuth();
 
   useEffect(() => {
-    fetch(`http://localhost:3000/v1/comments/${post.id}`)
-      .then((res) => res.json())
-      .then((data) => setComments(data));
+    const getData = async () => {
+      const response = await fetch(
+        `http://localhost:3000/v1/comments/${post.id}`,
+      );
+      const data = await response.json();
+      setComments(data.comments);
+    };
+    getData();
   }, [post.id]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch(
+      `http://localhost:3000/v1/posts/${post.id}/comment/`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+        body: JSON.stringify({ content: commentContent }),
+      },
+    );
+    const data = await response.json();
+    setComments((prev) => [{ ...data.newComment, user: { ...user } }, ...prev]);
+    setCommentContent("");
+  };
 
   return (
     <article className="post-card">
@@ -56,9 +82,36 @@ export const PostCard = ({ post }) => {
       <section
         className={`post-card-comments ${showComments ? "show" : "hide"}`}
       >
+        <form className="form-comment" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="comment..."
+            value={commentContent}
+            onChange={(e) => setCommentContent(e.target.value)}
+          />
+          <button>
+            <ion-icon name="send-outline"></ion-icon>
+          </button>
+        </form>
         {comments.map((comment) => (
           <div key={comment.id} className="comment">
-            <img src={post.user.image_url} alt={`foto de ${post.username}`} />
+            <section className="card-header">
+              <section className="card-header-left">
+                <img
+                  src={comment.user.image_url}
+                  alt={`foto de ${comment.username}`}
+                />
+                <section className="card-header-info">
+                  <Link
+                    className="relocation-user"
+                    to={`/${comment.user.username}`}
+                  >
+                    <h4>{comment.user.full_name}</h4>
+                  </Link>
+                  <p>@{comment.user.username}</p>
+                </section>
+              </section>
+            </section>
             <p>{comment.content}</p>
           </div>
         ))}
